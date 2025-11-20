582d19efa10f71a5332d812eeb4928196b21e292
Jake Kistler <Jake-Kistler@github.com>
2025-11-20 13:37:23 -0600

    Ooops rest of the Local GPU pipeline

--- TRMM_FIN/GPU/run_bench_op.sh
+++ TRMM_FIN/GPU/run_bench_op.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MIN=${1:-32}
-MAX=${2:-256}
+MAX=${2:-512}
 STEP=${3:-32}
 OBJ=./Obj-A
 
@@ -9,7 +9,5 @@ echo "Running benchmarks locally (WSL MPICH)"
 
 for X in $OBJ/*.run_bench.x; do
     echo "Bench: $X"
-    mpiexec -n 1 $X $MIN $MAX $STEP 1 -3 "$X.timing.csv"
-done
-
+    mpiexec -n 1 "$X" "$MIN" "$MAX" "$STEP" 1 -3 "${X}.timing.csv"
 done
